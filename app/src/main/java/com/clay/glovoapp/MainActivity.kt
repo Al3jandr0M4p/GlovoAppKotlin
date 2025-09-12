4d6ae58af5ba47e5654f2ffd1631d4aba4693ffd
Al3jandr0M4p <20230542@ipopsa.edu.do>
2025-09-12 18:07:16 -0400

    primer commit

---
+++ app/src/main/java/com/clay/glovoapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.clay.glovoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.clay.glovoapp.ui.theme.GlovoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GlovoAppTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
